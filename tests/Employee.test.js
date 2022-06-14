d0db302a81cafe2d55d08f23612d9da69f9aafce
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-06-14 15:42:50 -0400

    Added class files in lib

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require('../index');
+const Employee = require('../lib/employeeClass');
 
 describe('Employee', () => {
     describe('getName', () => {
@@ -7,6 +7,35 @@ describe('Employee', () => {
 
             const testEmployee = new Employee('Connor', 111111, 'connorkfitzsimmons@gmail.com');
             const result = testEmployee.getName();
+
+            expect(result).toBe(expected);
+        });
+    });
+    describe('getID', () => {
+        it('Should return the ID of the employee', () => {
+            const expected = 111111
+
+            const testEmployee = new Employee('Connor', 111111, 'connorkfitzsimmons@gmail.com');
+            const result = testEmployee.getID();
+
+            expect(result).toBe(expected);
+        });
+    });
+    describe('getEmail', () => {
+        it('Should return the email of the employee', () => {
+            const expected = 'connorkfitzsimmons@gmail.com'
+
+            const testEmployee = new Employee('Connor', 111111, 'connorkfitzsimmons@gmail.com');
+            const result = testEmployee.getEmail();
+
+            expect(result).toBe(expected);
+        });
+    });
+    describe('getRole', () => {
+        it('Should return the role of the employee', () => {
+            const expected = 'Employee'
+            const testEmployee = new Employee('Connor', 111111, 'connorkfitzsimmons@gmail.com');
+            const result = testEmployee.getRole();
 
             expect(result).toBe(expected);
         });
